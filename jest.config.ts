182f0f89d47a27b161ae92a7913a9b82b1d7f6fc
mehrabix <mehrabi@post.com>
2024-10-26 00:28:16 +0330

    Revert "chore: update angular"
    This reverts commit d085acebafc7e150a98e50fe260678e9c776ffed.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-import { getJestProjectsAsync } from '@nx/jest';
+import { getJestProjects } from '@nx/jest';
 
-export default async () => ({
-  projects: await getJestProjectsAsync(),
+export default {
+  projects: getJestProjects(),
   passWithNoTests: true,
-});
+};
